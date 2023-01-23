d1e138a57e5c963bebd9e492ef8ad0b7f609d0b1
ardit vezaj <avezaj3@gmail.com>
2023-01-23 13:52:50 +0100

    Update edit.blade.php

--- resources/views/todos/edit.blade.php
+++ resources/views/todos/edit.blade.php
@@ -56,6 +56,15 @@
                           class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                   </div>
                 </div>
+                <div>
+                    <label for="priority" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select priority</label>
+                    <select id="priority" name="priority" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option selected>Choose priority </option>
+                        @foreach ($priorities as $priority)
+                        <option> {{$priority}} </option>
+                        @endforeach
+                    </select>
+                </div>
 
                 <div>
                     <button type="submit"
