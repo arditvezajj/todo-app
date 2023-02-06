421f9a095445576c7db994eb04f5323eeb003583
ardit vezaj <avezaj3@gmail.com>
2023-02-06 14:00:35 +0100

    breeze packets

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,37 @@
 <?php
-
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TagController;
 use App\Http\Controllers\TodoController;
-use Illuminate\Support\Facades\Route;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('dashboard.home');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
 
 
 
@@ -13,9 +42,13 @@ Route::delete('/todo/{todo}', [Todocontroller::class, 'destroy']);
 Route::patch('/todo/{todo}', [TodoController::class, 'update']);
 Route::patch('/todo/{todo}/completed', [TodoController::class, 'completed']);
 
+
 //tags
 Route::get('/tags', [TagController::class, 'index'])->name('tag.index');
 Route::post('/tags', [TagController::class, 'store']);
 Route::delete('/tags/{tag}', [TagController::class, 'destroy']);
 Route::get('/tags/{tag}/edit', [TagController::class, 'edit']);
 Route::patch('/tags/{tag}', [TagController::class, 'update']);
+
+
+require __DIR__.'/auth.php';
