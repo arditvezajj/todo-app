c499d8dc106df964446b160259836db57d1b6582
ardit vezaj <avezaj3@gmail.com>
2023-02-13 17:47:24 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
+
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TagController;
 use App\Http\Controllers\TodoController;
+use App\Http\Controllers\ProjectController;
 
 
 
@@ -22,33 +24,40 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('dashboard.home');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    Route::get('/', [TodoController::class, 'index'])->name('todo.index');
+    Route::post('/todo', [TodoController::class, 'store']);
+    Route::get('/todo/edit/{todo}', [TodoController::class, 'edit']);
+    Route::delete('/todo/{todo}', [Todocontroller::class, 'destroy']);
+    Route::patch('/todo/{todo}', [TodoController::class, 'update']);
+    Route::patch('/todo/{todo}/completed', [TodoController::class, 'completed']);
+
+
+    //tags
+    Route::get('/tags', [TagController::class, 'index'])->name('tag.index');
+    Route::post('/tags', [TagController::class, 'store']);
+    Route::delete('/tags/{tag}', [TagController::class, 'destroy']);
+    Route::get('/tags/{tag}/edit', [TagController::class, 'edit']);
+    Route::patch('/tags/{tag}', [TagController::class, 'update']);
+
+   //project
+   Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
+
+
 });
 
 
 
 
 
-Route::get('/', [TodoController::class, 'index'])->name('todo.index');
-Route::post('/todo', [TodoController::class, 'store']);
-Route::get('/todo/edit/{todo}', [TodoController::class, 'edit']);
-Route::delete('/todo/{todo}', [Todocontroller::class, 'destroy']);
-Route::patch('/todo/{todo}', [TodoController::class, 'update']);
-Route::patch('/todo/{todo}/completed', [TodoController::class, 'completed']);
 
 
-//tags
-Route::get('/tags', [TagController::class, 'index'])->name('tag.index');
-Route::post('/tags', [TagController::class, 'store']);
-Route::delete('/tags/{tag}', [TagController::class, 'destroy']);
-Route::get('/tags/{tag}/edit', [TagController::class, 'edit']);
-Route::patch('/tags/{tag}', [TagController::class, 'update']);
-
-
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
