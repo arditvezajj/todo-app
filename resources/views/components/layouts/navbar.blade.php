8ccfd472af4e8dc67b106fdef731c310f50613a8
ardit vezaj <avezaj3@gmail.com>
2023-02-13 17:50:37 +0100

    make auth for log in

--- resources/views/components/layouts/navbar.blade.php
+++ resources/views/components/layouts/navbar.blade.php
@@ -37,31 +37,31 @@
                 </div>
                 <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
                     @auth
+
+
                         <x-layouts.navigation-link href="{{ route('todo.index') }}">
                             Todo
                         </x-layouts.navigation-link>
+                        
                     @endauth
                     @auth
-                    <x-layouts.navigation-link href="{{ route('tag.index') }}">
-                        Tags
-                    </x-layouts.navigation-link>
-                    @endauth
+                        <x-layouts.navigation-link href="{{ route('tag.index') }}">
+                            Tags
+                        </x-layouts.navigation-link>
+                    @endauth 
+                
+
+
+                     {{-- <x-layouts.navigation-link >
+                        Project
+                    </x-layouts.navigation-link>  --}}
+                
 
                     @guest
                         <x-layouts.navigation-link href="{{ route('register') }}">
                             Register
                         </x-layouts.navigation-link>
                     @endguest
-
-                    <x-layouts.navigation-link href="{{ route('login') }}">
-                        @auth
-                            Authenticated
-                        @endauth
-                    </x-layouts.navigation-link>
-
-
-
-
                 </div>
             </div>
             <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
@@ -98,7 +98,10 @@
             Leaving: "transition ease-in duration-75"
               From: "transform opacity-100 scale-100"
               To: "transform opacity-0 scale-95"
-          -->
+              
+          --> @auth {{ Auth::user()->name }}
+                    @endauth
+
                     <div x-show="navbarMenuOpen" x-cloak
                         class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                         role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
